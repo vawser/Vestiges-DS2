e0d04e3df3f1daa4ccdc860092f7e9675a059c4f
Vawser <ben.rouncefield@gmail.com>
2019-12-31 19:24:45 +0000

    Commit

---
+++ Tools/Localization/Repack.bat
@@ -0,0 +1,3 @@
+for %%i in ("Files\"*.xml) do "Yabber\Yabber.exe" "%%i"
+for %%i in ("Files\bloodmes\"*.xml) do "Yabber\Yabber.exe" "%%i"
+for %%i in ("Files\talk\"*.xml) do "Yabber\Yabber.exe" "%%i"
--- Vestiges/Copy to Game.bat
+++ Vestiges/Copy to Game.bat
@@ -1,2 +1,1 @@
 copy /b/v/y "enc_regulation.bnd.dcx" "E:\Program Files (x86)\SteamLibrary\steamapps\common\Dark Souls II Scholar of the First Sin\Game\enc_regulation.bnd.dcx"
-pause

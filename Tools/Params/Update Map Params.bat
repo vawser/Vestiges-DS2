78418dc572ef724f5a74a37dcd6ae3a9fda30efb
Vawser <ben.rouncefield@gmail.com>
2020-01-02 22:41:08 +0000

    Research
    Complete labelling for enemy generator and registration params

--- Tools/Params/Update Map Params.bat
+++
@@ -1,6 +0,0 @@
-"Yabber\Yabber.exe" "map.bnd.dcx"
-
-del "E:\Program Files (x86)\SteamLibrary\steamapps\common\Dark Souls II Scholar of the First Sin\Modding\Mod\Tools\Params\map-bnd-dcx\_yabber-bnd4.xml"
-
-copy /y /v "E:\Program Files (x86)\SteamLibrary\steamapps\common\Dark Souls II Scholar of the First Sin\Modding\Mod\Tools\Params\map-bnd-dcx\" "E:\Program Files (x86)\SteamLibrary\steamapps\common\Dark Souls II Scholar of the First Sin\Modding\Mod\Vestiges\param\" 
-

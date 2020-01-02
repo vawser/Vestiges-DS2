fb4b38440404b11249e7a12a1f4f6d24ad879038
Vawser <ben.rouncefield@gmail.com>
2020-01-02 00:03:59 +0000

    Research

---
+++ Tools/Params/Update Mod and Game.bat
@@ -0,0 +1,10 @@
+copy /b/v/y "enc_regulation.bnd.dcx" "E:\Program Files (x86)\SteamLibrary\steamapps\common\Dark Souls II Scholar of the First Sin\Modding\Mod\Vestiges\enc_regulation.bnd.dcx"
+
+copy /y /v "E:\Program Files (x86)\SteamLibrary\steamapps\common\Dark Souls II Scholar of the First Sin\Modding\Mod\Vestiges\" "E:\Program Files (x86)\SteamLibrary\steamapps\common\Dark Souls II Scholar of the First Sin\Game\" 
+
+copy /y /v "E:\Program Files (x86)\SteamLibrary\steamapps\common\Dark Souls II Scholar of the First Sin\Modding\Mod\Vestiges\map\" "E:\Program Files (x86)\SteamLibrary\steamapps\common\Dark Souls II Scholar of the First Sin\Game\map\" 
+
+copy /y /v "E:\Program Files (x86)\SteamLibrary\steamapps\common\Dark Souls II Scholar of the First Sin\Modding\Mod\Vestiges\menu\" "E:\Program Files (x86)\SteamLibrary\steamapps\common\Dark Souls II Scholar of the First Sin\Game\menu\" 
+
+copy /y /v "E:\Program Files (x86)\SteamLibrary\steamapps\common\Dark Souls II Scholar of the First Sin\Modding\Mod\Vestiges\param\" "E:\Program Files (x86)\SteamLibrary\steamapps\common\Dark Souls II Scholar of the First Sin\Game\param\" 
+
--- Vestiges/Copy to Game.bat
+++
@@ -1,1 +0,0 @@
-copy /b/v/y "enc_regulation.bnd.dcx" "E:\Program Files (x86)\SteamLibrary\steamapps\common\Dark Souls II Scholar of the First Sin\Game\enc_regulation.bnd.dcx"

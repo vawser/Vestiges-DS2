ceb18237725ff5d190807e9e2c4d1a21dadbb421
Vawser <ben.rouncefield@gmail.com>
2020-01-06 13:27:21 +0000

    Mod Setup
    Adjusted stuff to account for Mod Engine.

---
+++ Tools/Params/Pull Map Changes.bat
@@ -0,0 +1,4 @@
+:: Copy changed map params into params-bnd-dcx
+copy /b/v/y "E:\Program Files (x86)\SteamLibrary\steamapps\common\Dark Souls II Scholar of the First Sin\Modding\Mod\Vestiges\param\" "E:\Program Files (x86)\SteamLibrary\steamapps\common\Dark Souls II Scholar of the First Sin\Modding\Mod\Tools\Params\params-bnd-dcx\" 
+
+
--- Tools/Params/Update Params.bat
+++ Tools/Params/Update Params.bat
@@ -6,9 +6,3 @@ copy /b/v/y "E:\Program Files (x86)\SteamLibrary\steamapps\common\Dark Souls II 
 :: Delete the yabber xml file from the Vestiges param folder
 del /q "E:\Program Files (x86)\SteamLibrary\steamapps\common\Dark Souls II Scholar of the First Sin\Modding\Mod\Vestiges\param\_yabber-bnd4.xml"
 
-:: Copy param files to Game param folder
-copy /b/v/y "E:\Program Files (x86)\SteamLibrary\steamapps\common\Dark Souls II Scholar of the First Sin\Modding\Mod\Tools\Params\params-bnd-dcx\" "E:\Program Files (x86)\SteamLibrary\steamapps\common\Dark Souls II Scholar of the First Sin\Game\Param\"
-
-:: Delete the yabber xml file from the Game param folder
-del /q "E:\Program Files (x86)\SteamLibrary\steamapps\common\Dark Souls II Scholar of the First Sin\Game\Param\_yabber-bnd4.xml"
-

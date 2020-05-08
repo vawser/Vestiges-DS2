94f1dcbf63ae23ef0ed0e8c8a2d8a3989586377e
Vawser <ben.rouncefield@gmail.com>
2020-05-08 05:42:40 +0100

    Update
    Partially added latest names from Paramdex

--- Tools/EMEVD/Tool/Extract.bat
+++ Tools/EMEVD/Tool/Extract.bat
@@ -1,22 +1,22 @@
 est ..\SpEffectAbnormalState.emevd -n -o SpEffectAbnormalState.txt
 est ..\SpEffectAbnormalState.emevd -v -o SpEffectAbnormalState.verbose.txt
-est ..\SpEffectActiveItem.emevd -n -o SpEffectActiveItem.txt
-est ..\SpEffectActiveItem.emevd -v -o SpEffectActiveItem.verbose.txt
-REM est ..\SpEffectArmor.emevd -n -o SpEffectArmor.txt
-REM est ..\SpEffectArmor.emevd -v -o SpEffectArmor.verbose.txt
-REM est ..\SpEffectCondition.emevd -n -o SpEffectCondition.txt
-REM est ..\SpEffectCondition.emevd -v -o SpEffectCondition.verbose.txt
-est ..\SpEffectEnemy.emevd -n -o SpEffectEnemy.txt
-est ..\SpEffectEnemy.emevd -v -o SpEffectEnemy.verbose.txt
-est ..\SpEffectMustSub.emevd -n -o SpEffectMustSub.txt
-est ..\SpEffectMustSub.emevd -v -o SpEffectMustSub.verbose.txt
-est ..\SpEffectPassiveItem.emevd -n -o SpEffectPassiveItem.txt
-est ..\SpEffectPassiveItem.emevd -v -o SpEffectPassiveItem.verbose.txt
-REM est ..\SpEffectRing.emevd -n -o SpEffectRing.txt
-REM est ..\SpEffectRing.emevd -v -o SpEffectRing.verbose.txt
-est ..\SpEffectSpell.emevd -n -o SpEffectSpell.txt
-est ..\SpEffectSpell.emevd -v -o SpEffectSpell.verbose.txt
-est ..\SpEffectWeapon.emevd -n -o SpEffectWeapon.txt
-est ..\SpEffectWeapon.emevd -v -o SpEffectWeapon.verbose.txt
-est ..\SpEffectWideUse.emevd -n -o SpEffectWideUse.txt
-est ..\SpEffectWideUse.emevd -v -o SpEffectWideUse.verbose.txt
+REM est ..\SpEffectActiveItem.emevd -n -o SpEffectActiveItem.txt
+REM est ..\SpEffectActiveItem.emevd -v -o SpEffectActiveItem.verbose.txt
+REM REM est ..\SpEffectArmor.emevd -n -o SpEffectArmor.txt
+REM REM est ..\SpEffectArmor.emevd -v -o SpEffectArmor.verbose.txt
+REM REM est ..\SpEffectCondition.emevd -n -o SpEffectCondition.txt
+REM REM est ..\SpEffectCondition.emevd -v -o SpEffectCondition.verbose.txt
+REM est ..\SpEffectEnemy.emevd -n -o SpEffectEnemy.txt
+REM est ..\SpEffectEnemy.emevd -v -o SpEffectEnemy.verbose.txt
+REM est ..\SpEffectMustSub.emevd -n -o SpEffectMustSub.txt
+REM est ..\SpEffectMustSub.emevd -v -o SpEffectMustSub.verbose.txt
+REM est ..\SpEffectPassiveItem.emevd -n -o SpEffectPassiveItem.txt
+REM est ..\SpEffectPassiveItem.emevd -v -o SpEffectPassiveItem.verbose.txt
+REM REM est ..\SpEffectRing.emevd -n -o SpEffectRing.txt
+REM REM est ..\SpEffectRing.emevd -v -o SpEffectRing.verbose.txt
+REM est ..\SpEffectSpell.emevd -n -o SpEffectSpell.txt
+REM est ..\SpEffectSpell.emevd -v -o SpEffectSpell.verbose.txt
+REM est ..\SpEffectWeapon.emevd -n -o SpEffectWeapon.txt
+REM est ..\SpEffectWeapon.emevd -v -o SpEffectWeapon.verbose.txt
+REM est ..\SpEffectWideUse.emevd -n -o SpEffectWideUse.txt
+REM est ..\SpEffectWideUse.emevd -v -o SpEffectWideUse.verbose.txt
